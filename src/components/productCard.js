6415c338247374fe810903abed0a027bfa0a3eb8
muhammednasimda <muhammednasimda@gmail.com>
2020-11-18 13:26:30 +0530

    fixed everything link

--- src/components/productCard.js
+++ src/components/productCard.js
@@ -1,27 +1,28 @@
 import React, { useState } from "react";
 import styles from "../pages/css/products.module.scss";
-import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Product = ({ productList }) => {
   const [selectedProduct, setSelected] = useState(0);
   const selectProduct = (id) => {
     setSelected(id);
   };
+  let history = useHistory();
   const productDivList = productList.map((product) => {
     return (
-      <Link
-        to="/productedit"
+      <div
         className={`${styles["product-card"]} ${
           selectedProduct === product.id ? styles.active : ""
         }`}
         key={product.id}
         onClick={() => {
           selectProduct(product.id);
+          history.push("/productedit");
         }}
       >
         <img src={product.productImage} alt="product" />
         <h4>{product.productName}</h4>
-      </Link>
+      </div>
     );
   });
   return productDivList;
