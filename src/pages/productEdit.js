b4ecce148377c3fe2869b8804f75c25465a5191d
muhammednasimda <muhammednasimda@gmail.com>
2020-11-19 10:44:45 +0530

    cleaned up editor

--- src/pages/productEdit.js
+++ src/pages/productEdit.js
@@ -2,22 +2,34 @@ import React, { useState } from "react";
 import Draggable from "react-draggable";
 import styles from "./css/productedit.module.scss";
 import Slider from "react-input-slider";
+import mockup from "./iphone11.png";
 
 const ProductEdit = () => {
-  let [imageWidth, setWidth] = useState(0);
+  let [imageWidth, setWidth] = useState(200);
   return (
     <div className={styles.mainContainer}>
-      <div className={styles.main}>
-        <Draggable bounds="parent">
-          <img alt="tee" src="http://picsum.photos/100" width={imageWidth} />
-        </Draggable>
+      <div className={styles.editContainer}>
+        <div className={styles.dragContainer}>
+          <Draggable>
+            <img
+              alt="tee"
+              src="http://picsum.photos/800"
+              width={imageWidth}
+              className={styles.customImage}
+            />
+          </Draggable>
+        </div>
+        <img src={mockup} className={styles.mockupImage} />
       </div>
-      <Slider
-        axis="x"
-        xmax="400"
-        x={imageWidth}
-        onChange={({ x }) => setWidth(x)}
-      />
+      <div className={styles.controlsContainer}>
+        <p>Image Size</p>
+        <Slider
+          axis="x"
+          xmax="600"
+          x={imageWidth}
+          onChange={({ x }) => setWidth(x)}
+        />
+      </div>
     </div>
   );
 };
